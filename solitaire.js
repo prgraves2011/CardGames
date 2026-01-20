57293b4091b215fc7f465e85feaaf5fc6ba18a2a
Peter Graves <pr.graves.2011@gmail.com>
2026-01-20 17:31:23 +0000

    Logo added to home page.

--- solitaire.js
+++ solitaire.js
@@ -1,1 +1,38 @@
 
+// card rank values
+const rank_val = {
+    A: 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10,
+    J: 11,
+    Q: 12,
+    K: 13,
+};
+
+// suit colours
+const suitColor = {
+    C: 'B',
+    S: 'B',
+    D: 'R',
+    H: 'R',
+};
+
+// reset game button
+document.getElementById('reset-btn').addEventListener('click', () => {
+    dealSolitaire();
+});
+
+// deal freecell game
+function dealSolitaire() {
+
+    // create cards
+    const suits = ['H', 'D', 'C', 'S'];
+    const ranks = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+}
